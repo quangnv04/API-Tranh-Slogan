cfae751727949c43af01bdd68cb6251b346ed804
quangnv04 <ngovanquang2601@gmail.com>
2025-04-09 02:41:02 +0700

    Implement order management sync orders from Google Sheets

--- app/routers/orders.py
+++ app/routers/orders.py
@@ -15,19 +15,3 @@ async def get_orders(request: Request):
         time_debug = time.time()
     return templates.TemplateResponse("order.html", {"request": request, "time": time_debug})
 
-@router.get("/order/{id}", response_class=HTMLResponse)
-async def get_order_detail(request: Request, id: str, db: SQLite3Connection = Depends(get_db)):
-    time_debug = "0.3"
-    if DEBUG == 'True':
-        time_debug = time.time()
-
-    order = await run_in_threadpool(OrdersModel(db).get_orders_by_id, id)
-
-    if order is None:
-        return RedirectResponse(url='/orders', status_code=303)
-
-    return templates.TemplateResponse("orders-detail.html", {
-        "request": request,
-        "order": order,
-        "time": time_debug
-    })
